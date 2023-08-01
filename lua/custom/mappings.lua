c50fb52a2b4014d0bbb7e1339369454519b85b12
importedgallo <andre.mongard@gmail.com>
2023-08-01 22:49:25 +1000

    removed custom from .gitignore

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    -- debug breakpoint mapping
+    ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>"}
+  }
+}
+
+M.dap_python = {
+  plugin = true,
+  n = {
+    -- debug python run
+    -- brings up dap-ui
+    ["<leader>dpr"] = {
+      function()
+        require('dap-python').test_method()
+      end
+    }
+  }
+}
+
+return M
